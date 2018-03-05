ed7d8e975ed6f7d8496d14207534a9eaa13730d7
jbishop-art <jonathan.bishop.art@gmail.com>
2018-03-04 19:57:48 -0700

    Initial char jump and camera follow established.

---
+++ Virtual Worlds Side Scroller/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public float ZPos;
+	private float XPos;
+	private float YPos;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		XPos = GameObject.Find("Player").transform.position.x;
+		YPos = GameObject.Find("Player").transform.position.y;
+
+		transform.position = new Vector3(XPos, YPos, ZPos);
+
+	}
+}
