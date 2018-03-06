4aa6ccb02e7b2f32268c9ce0fe9dc3d134c609e8
jbishop-art <jonathan.bishop.art@gmail.com>
2018-03-06 15:33:31 -0700

    CheckPoint system made.

--- Virtual Worlds Side Scroller/Assets/Scripts/HitBadStuff.cs
+++ Virtual Worlds Side Scroller/Assets/Scripts/HitBadStuff.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class HitBadStuff : MonoBehaviour {
+public class HitBadStuff : MonoBehaviour
+{
+    private Vector3 spawnPoint;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start ()
+    {
+
+                
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
+        //Detects if player touches a spawn point.
+        if (col.gameObject.tag == "CheckPoint")
+        {
+            spawnPoint = col.gameObject.transform.position;
+
+            Destroy(col.gameObject);
+        }
+
+        
 		//Detects if player touches BadStuff.
 		if(col.gameObject.tag == "BadStuff")
 		{
-			
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-			
-		}
 
+            GameObject.Find("Player(Clone)").transform.position = spawnPoint;
+
+        }
+
+
+        
 		
 	}
 
